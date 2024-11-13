4c12c19e95c574fc14ff56bce995fb591a91b648
Sowmithra Suresh Kumar <sowmithra.sureshkuma@ucalgary.ca>
2024-11-13 00:08:56 -0700

    Deleted my fxml files, added in planning documents from gui-team branch

--- src/main/java/ca/ucalgary/cpsc/projectguiv1/HelloApplication.java
+++ src/main/java/ca/ucalgary/cpsc/projectguiv1/HelloApplication.java
@@ -10,13 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("log-in.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-
-        SceneController screenController = new SceneController(scene); // set main
-        screenController.addScreen("signup", FXMLLoader.load(HelloApplication.class.getResource( "sign-up.fxml")));
-        screenController.addScreen("reset", FXMLLoader.load(HelloApplication.class.getResource( "reset-password.fxml")));
-
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/ca/ucalgary/cpsc/projectguiv1/MainController.java
+++ src/main/java/ca/ucalgary/cpsc/projectguiv1/MainController.java
@@ -12,32 +12,8 @@ public class MainController {
     private Label welcomeText;
 
     @FXML
-    protected void onLoginButtonClick() {
+    protected void onHelloButtonClick() {
         welcomeText.setText("New Text!");
     }
-
-    @FXML
-    protected void onResetPasswordLinkClick() throws IOException {
-        // Load reset password page
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("log-in.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        SceneController screenController = new SceneController(scene); // set main
-        //screenController.addScreen("signup", FXMLLoader.load(HelloApplication.class.getResource( "sign-up.fxml")));
-        screenController.activate("reset");
-    }
-
-    @FXML
-    protected void onSignUpLinkClicked() {
-        // on sign up link clicked
-        welcomeText.setText("New Text!");
-    }
-
-    @FXML
-    protected void onResetBackButtonClick() {
-        // go back to log in page
-        welcomeText.setText("New Text!");
-    }
-
-    // After login button is clicked, redirect to home page
 
 }
