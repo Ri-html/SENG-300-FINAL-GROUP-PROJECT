a0d4265f10e5b96755a58e8470500e0273a6ce8c
Harry He <ri.he1@ucalgary.ca>
2024-11-22 22:14:36 -0700

    had boardgame inherit from game

--- src/gameLogic/boardGames/BoardGame.java
+++ src/gameLogic/boardGames/BoardGame.java
@@ -2,7 +2,7 @@ package gameLogic.boardGames;
 
 import gameLogic.piece.AbstractPiece;
 
-public interface BoardGame {
+public interface BoardGame extends Game{
     void placeBoardPiece(AbstractPiece piece, int x, int y);
 
     void setUpBoard();
--- src/gameLogic/boardGames/Game.java
+++ src/gameLogic/boardGames/Game.java
@@ -1,4 +1,4 @@
-package gameLogic;
+package gameLogic.boardGames;
 
 import gameLogic.player.AbstractPlayer;
 
