6aeb3a277271cb4dc627061850b7b334e4249474
Harry He <ri.he1@ucalgary.ca>
2024-11-22 22:39:23 -0700

    moved everything in Game interface into abstract board game

--- src/gameLogic/boardGames/Game.java
+++ src/gameLogic/boardGames/Game.java
@@ -3,9 +3,4 @@ package gameLogic.boardGames;
 import gameLogic.player.AbstractPlayer;
 
 public interface Game {
-    boolean validateVictory(); //interface methods public by default
-
-    void setCurrentPlayer();
-
-    void addPlayer(AbstractPlayer player);
 }
