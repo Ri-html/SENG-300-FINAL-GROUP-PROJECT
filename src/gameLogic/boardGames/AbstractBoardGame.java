f22a126aaae523822fb959033624358941391811
Harry He <ri.he1@ucalgary.ca>
2024-11-23 16:01:42 -0700

    started coding abstract board game

--- src/gameLogic/boardGames/AbstractBoardGame.java
+++ src/gameLogic/boardGames/AbstractBoardGame.java
@@ -6,18 +6,26 @@ import gameLogic.player.AbstractPlayer;
 
 import java.util.ArrayList;
 
-public class AbstractBoardGame implements BoardGame {
+public abstract class AbstractBoardGame implements BoardGame {
     public ArrayList<BoardGameObserver> observers;
     private String winner;
     private String gameID;
-    private AbstractPlayer currentPlayer;
-    private AbstractPlayer[] players;
+    private String currentPlayer;
+    private String[] players;
     protected Piece[][] gameBoard;
+    int addedPlayers=0;
+    int playerNumber;
+    private GameState gameState;
+
+    public AbstractBoardGame(int playerNum) {
+        gameState = GameState.WAITING;
+        playerNumber = playerNum;
+        players = new String[playerNum];
+    }
 
     public void attach(BoardGameObserver observer){
         observers.add(observer);
     }
-
     public void detach(BoardGameObserver observer){
         observers.remove(observer);
     }
@@ -42,4 +50,36 @@ public class AbstractBoardGame implements BoardGame {
     @Override
     public void setUpBoard() {
     }
+
+    abstract public boolean validateVictory();
+
+    public void setCurrentPlayer(String aCurrentPlayer){
+        currentPlayer=aCurrentPlayer;
+    }
+
+    public void addPlayer(String player) {
+        if (addedPlayers>=playerNumber){
+            System.err.println("Player is at full capacity");
+        } else if (addedPlayers==playerNumber-1) {
+            players[addedPlayers++]=player;
+            gameState=GameState.INPROGRESS;
+        } else{
+            players[addedPlayers++]=player;
+        }
+    }
+
+    public String getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+}
+
+enum GameState {
+    WAITING,
+    INPROGRESS,
+    OVER
 }
