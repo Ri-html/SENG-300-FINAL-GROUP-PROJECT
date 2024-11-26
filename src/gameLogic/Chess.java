c2cfecd350d0ca4c3b998d743f3b961c6c2c2a23
james.clark4@ucalgary.ca <james.clark4@ucalgary.ca>
2024-11-26 16:20:49 -0700

    30213187 work on chess board setup

--- src/gameLogic/Chess.java
+++ src/gameLogic/Chess.java
@@ -5,6 +5,7 @@ import gameLogic.boardGames.BoardGameObserver;
 import gameLogic.piece.Piece;
 import gameLogic.player.ChessPlayer;
 import gameLogic.piece.chessPiece.*;
+import gameLogic.side.ChessSide;
 
 import java.util.ArrayList;
 
@@ -34,12 +35,32 @@ public class Chess extends AbstractBoardGame {
 
     @Override
     protected void setUpBoard(Piece[][] board) {
-        //add pieces to board
-        board[0][0] =;
-        board[0][1] =;
-            //add pawns
+        //add white pieces to board
+        board[0][0] = new Rook(ChessSide.WHITE);
+        board[0][1] = new Knight(ChessSide.WHITE);
+        board[0][2] = new Bishop(ChessSide.WHITE);
+        board[0][3] = new Queen(ChessSide.WHITE);
+        board[0][4] = new King(ChessSide.WHITE);
+        board[0][5] = new Bishop(ChessSide.WHITE);
+        board[0][6] = new Knight(ChessSide.WHITE);
+        board[0][7] = new Rook(ChessSide.WHITE);
+        //add white pawns
         for (int i = 0; i < 8; i++) {
-            board[1][i] = new Pawn();
+            board[1][i] = new Pawn(ChessSide.WHITE);
+        }
+        //add black pieces to board
+        board[7][0] = new Rook(ChessSide.BLACK);
+        board[7][1] = new Knight(ChessSide.BLACK);
+        board[7][2] = new Bishop(ChessSide.BLACK);
+        board[7][3] = new Queen(ChessSide.BLACK);
+        board[7][4] = new King(ChessSide.BLACK);
+        board[7][5] = new Bishop(ChessSide.BLACK);
+        board[7][6] = new Knight(ChessSide.BLACK);
+        board[7][7] = new Rook(ChessSide.BLACK);
+        //add black pawns
+        for (int i = 0; i < 7; i++) {
+            board[7][i] = new Pawn(ChessSide.BLACK);
+
         }
     }
 
