0042125c403866121cb1d8c806574cb9a357efb1
Harry He <ri.he1@ucalgary.ca>
2024-11-26 22:32:02 -0700

    30221727:finished test for addPlayer

--- test/gameLogic/boardGames/AbstractBoardGameTest.java
+++ test/gameLogic/boardGames/AbstractBoardGameTest.java
@@ -1,9 +1,48 @@
 package gameLogic.boardGames;
+import gameLogic.piece.Piece;
+import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.*;
 
 
 public class AbstractBoardGameTest {
-
+    SomeBoardGame boardGame;
+    @Before
+    public void resetBoardGame(){
+        boardGame=new SomeBoardGame(2);
+    }
+    @Test
+    public void addPlayerTest(){
+        boardGame.addPlayer("rerere");
+        assertEquals(boardGame.getGameState(), AbstractBoardGame.GameState.WAITING);
+        boardGame.addPlayer("rerere");
+        assertEquals(boardGame.getGameState(), AbstractBoardGame.GameState.INPROGRESS);
+        boardGame.addPlayer("rerere");
+    }
 }
 
+class SomeBoardGame extends AbstractBoardGame {
 
+    public SomeBoardGame(int playerNum) {
+        super(playerNum);
+    }
+
+    @Override
+    protected void setUpBoard(Piece[][] board) {
+    }
+
+    @Override
+    public GameEndState validateGameEnds() {
+        return null;
+    }
+
+    @Override
+    public boolean validateMove(int[] moves) {
+        return false;
+    }
+
+    @Override
+    public void makeMove(int[] moves) {
+
+    }
+}
