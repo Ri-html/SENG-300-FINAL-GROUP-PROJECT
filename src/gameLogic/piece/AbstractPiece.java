66ad1d37fd861faa4c3bf6527fc9a7fc5693b28f
Harry He <ri.he1@ucalgary.ca>
2024-11-26 21:14:42 -0700

    30221727:simplified abstractPiece to just include size and display

--- src/gameLogic/piece/AbstractPiece.java
+++ src/gameLogic/piece/AbstractPiece.java
@@ -1,35 +1,17 @@
 package gameLogic.piece;
 
+import gameLogic.side.Side;
+
 public abstract class AbstractPiece implements Piece {
-    private int[] location; //should this be an x, y?
-    private ;
-
-
-    public int[] getLocation() {
-        return location;
+    private final Side side;
+    private String display;
+    public AbstractPiece(Side side, String display) {
+        this.side = side;
+        this.display = display;
     }
 
-    public void setLocation(int[] location) {
-        this.location = location;
+    public Side getSide() {
+        return side;
     }
 
-    public void moveLocation(){ //can we not just use setLocation for this?
-    }
-
-    public void setType(){
-    }
-
-    public PieceType getType(){
-        return null;
-    }
-
-    public void getValidMoves(){ //why is this void in class diagram?
-                                // maybe return array of tuples with all possible moves x,y
-    }
-
-    public void updateValidMoves(){
-    }
-
-    public void setPlayer(String player){
-    }
 }
