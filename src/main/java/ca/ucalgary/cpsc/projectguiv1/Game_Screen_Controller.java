006a2a469eadbfc22e523595431a8a8a66cff596
Harry He <ri.he1@ucalgary.ca>
2024-11-29 18:26:33 -0700

    30221727: set the 3 game screen to have the same controller

---
+++ src/main/java/ca/ucalgary/cpsc/projectguiv1/Game_Screen_Controller.java
@@ -0,0 +1,19 @@
+package ca.ucalgary.cpsc.projectguiv1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class Game_Screen_Controller {
+
+    //Shared function
+    @FXML
+    Button exit;
+    @FXML
+    AnchorPane afterlife1;
+    AnchorPane afterlife2;
+
+    //Chess Specific
+    //Tic Tac Toe Specific
+    //Checkers Specific
+}
