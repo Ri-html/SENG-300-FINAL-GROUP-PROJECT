f77d33c1f67ad70ce2259213e27b14bcda278a7e
Jonathan Holst <97708227+JonathanNoH@users.noreply.github.com>
2024-11-29 19:45:46 -0700

    30038361: add javadocs

--- src/gameLogic/ConnectFour.java
+++ src/gameLogic/ConnectFour.java
@@ -11,20 +11,34 @@ public class ConnectFour extends AbstractBoardGame {
         super(playerNum);
     }
 
+    /**
+     * Takes a player id and column and places a piece in that column
+     * @param int player id
+     * @param int x for column to place in
+     * @return bool success
+     */
     private boolean placePiece(int player, int x) {
         isValid = isValidMove(x);
-        for (int i = board[0].length-1; i >= 0; i--) {
-            if (board[i][x] == null) {
-                if (player1 == player) {
-                    board[i][x] = new ConnectFourPiece(ConnectFourSide.RED);
-                } else {
-                    board[i][x] = new ConnectFourPiece(ConnectFourSide.YELLOW);
+        if (isValid) {
+            for (int i = board[0].length - 1; i >= 0; i--) {
+                if (board[i][x] == null) {
+                    if (player1 == player) {
+                        board[i][x] = new ConnectFourPiece(ConnectFourSide.RED);
+                    } else {
+                        board[i][x] = new ConnectFourPiece(ConnectFourSide.YELLOW);
+                    }
+                    return true;
                 }
-                return;
             }
+        } else {
+            return false;
         }
     }
 
+    /**
+     * Checks if win has been reached
+     * @return bool true on win or false on no win
+     */
     public boolean checkWin() {
         if (checkHorizontalWin() || checkVerticalWin() || checkDiagonalWin()) {
             return true;
@@ -32,18 +46,34 @@ public class ConnectFour extends AbstractBoardGame {
         return false;
     }
 
+    /**
+     * Checks board for win horizontally
+     * @return true on win horizontal or false on no win
+     */
     private boolean checkHorizontalWin() {
         //!TODO
     }
 
+    /**
+     * Checks board for win vertically
+     * @return true on win vertical or false on no win
+     */
     private boolean checkVerticalWin() {
         //!TODO
     }
 
+    /**
+     * Checks board for win diagonally
+     * @return true on win diagonal or false on no win
+     */
     private boolean checkDiagonalWin() {
         //!TODO
     }
 
+    /**
+     * Checks if board is full (no possible moves left)
+     * @return true if board is full
+     */
     private boolean isFull() {
         boolean full = true;
         for ( i=0; i < board[0].length; i++) {
@@ -54,39 +84,70 @@ public class ConnectFour extends AbstractBoardGame {
         return full;
     }
 
-    public char[][] getBoard() {
+    /**
+     * Returns board state
+     */
+    public ConnectFourPiece[][] getBoard() {
         return board;
     }
 
+    /**
+     * Checks if valid column to make move
+     * @param x int of column to place
+     */
     private boolean isValidMove(int x) {
         return board[0][x] == null;
     }
-*/
+
+    /**
+     * checks valid move
+     * @param x checks column to place in
+     * @param y ignored, artifact of abstract
+     */
     public void checkMoves(int x, int y) {
         return isValidMove(x);
     }
 
+    /**
+     * Creates a new board for game
+     * @return 2d array of ConnectFourPieces of the current board state
+     */
     @Override
     protected Piece[][] setUpBoard() {
-        return new Piece[3][3];
+        this.board = new ConnectFourPiece[6][7];
+        return getBoard();
     }
 
+    /**
+     * check for a game ending condition
+     * @return GameEndState enum
+     */
     @Override
     public GameEndState validateGameEnds() {
         if (checkWin()) {
-            return GameEndState.Victory;
+            return GameEndState.VICTORY;
         } else if (isFull()){
             return GameEndState.DRAW;
         } else {
-            return GameEndState.Ongoing;
+            return GameEndState.ONGOING;
         }
     }
 
+    /**
+     * Check if move requested is legal
+     * @param moves coords of move to place
+     * @return true if legal move
+     */
     @Override
     public boolean validateMove(int[] moves) {
         isValidMove(moves[0]);
     }
 
+    /**
+     * Place move requested (does nothing if invalid move)
+     * @param player1 id of player making move
+     * @param moves coords of move
+     */
     @Override
     public void makeMove(int player1, int[] moves) {
         if (validateMove(int[] moves)){
