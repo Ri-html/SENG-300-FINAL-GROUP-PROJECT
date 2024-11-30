243d3cb0466a98c6fdeac38030df5e4e7aa4c280
james.clark4@ucalgary.ca <james.clark4@ucalgary.ca>
2024-11-30 14:43:41 -0700

    30213187 chess piece logic

--- src/gameLogic/piece/chessPiece/King.java
+++ src/gameLogic/piece/chessPiece/King.java
@@ -2,6 +2,8 @@ package gameLogic.piece.chessPiece;
 
 import gameLogic.side.ChessSide;
 
+import java.sql.Array;
+
 public class King extends AbstractChessPiece{
     private ChessSide side;
     public King(ChessSide side) {
@@ -11,6 +13,23 @@ public class King extends AbstractChessPiece{
 
     @Override
     public int[][] getValidMoves() {
-        return new int[0][];
+        int[][] squaresCanMove = new int[8][2];
+        int y = this.getLocation()[0];
+        int x = this.getLocation()[1];
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                //filter out current position as it doesn't take a move
+                if(!(x==0 && y==0)){
+                    //check piece is in bounds of board
+                    if(x+i <=0 && x+i <= 7){
+                        if(y+j <=0 && y+j <= 7){
+
+                        }
+                    }
+                }
+            }
+        }
+        int[][] validMoves = new int[8][2];
+
     }
 }
