cb1c157365a8416dfcc721903e54fe9c61c44310
Sean Skelton <sean.skelton@ucalgary.ca>
2024-11-30 23:09:30 -0700

    00334798 Added knight move check

--- src/gameLogic/piece/chessPiece/Knight.java
+++ src/gameLogic/piece/chessPiece/Knight.java
@@ -1,6 +1,8 @@
 package gameLogic.piece.chessPiece;
 import gameLogic.side.ChessSide;
 
+import java.util.*;
+
 public class Knight extends AbstractChessPiece{
     private ChessSide side;
     public Knight(ChessSide side) {
@@ -10,7 +12,27 @@ public class Knight extends AbstractChessPiece{
 
     @Override
     public int[][] getValidMoves() {
-        return new int[0][];
+
+        final int[][] KNIGHT_MOVES = {
+                {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
+                {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
+        };
+
+        List<int[]> validMoves = new ArrayList<>();
+
+        int x = this.getLocation()[0];
+        int y = this.getLocation()[1];
+
+        for (int[] move : KNIGHT_MOVES) {
+            int newX = x + move[0];
+            int newY = y + move[1];
+
+            if (isInBounds(newX, newY) && isValidSquare(newX, newY)) {
+                validMoves.add(new int[] {newX, newY});
+            }
+        }
+
+        return validMoves.toArray(new int[validMoves.size()][2]);
     }
 
 }
