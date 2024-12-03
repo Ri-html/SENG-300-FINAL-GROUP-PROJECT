42ff17e30226a4c6a3185a34fc9ab366e64db723
james.clark4@ucalgary.ca <james.clark4@ucalgary.ca>
2024-12-03 16:47:02 -0700

    30213187 move pawn tests to new file

--- test/gameLogic/boardGames/ChessTest.java
+++ test/gameLogic/boardGames/ChessTest.java
@@ -61,45 +61,5 @@ public class ChessTest {
         }
     }
 
-    //Test pawn valid moves, might not need this
-    @Test
-    public void pawnValidMovesTest(){
-        Chess chess = new Chess();
-        AbstractChessPiece[][] testBoard = new AbstractChessPiece[8][8];
-        testBoard[1][2] = new Pawn(ChessSide.WHITE);
-        //its valid moves should be to (2,2) and (3,2)
-        //testBoard[1][2].
-    }
-
-    //Test pawn movement
-    @Test
-    public void pawnMoveTest1(){
-        //this fails because getValidMoves() returns null
-        Chess chess = new Chess();
-        AbstractChessPiece[][] board = new AbstractChessPiece[8][8];
-        board[1][2] = new Pawn(ChessSide.WHITE);
-        //check initial double move forward
-        board[1][2].makeMove(board, 3, 2);
-        //check old location has been cleared correctly
-        //is an empty square null or something else?
-        assertNull(board[1][2]);
-        assertNotNull(board[3][2]); //check piece has moved to (3,2)
-        //check single move forward
-
-    }
-
-    //Test pawn capturing
-    @Test
-    public void pawnMoveTest2(){
-        Chess chess = new Chess();
-        AbstractChessPiece[][] board = new AbstractChessPiece[8][8];
-        board[1][2] = new Pawn(ChessSide.WHITE);
-        board[6][3] = new Pawn(ChessSide.BLACK);
-        //test pawn capturing
-
-        //test capture by en passant rule
-    }
-
-
 
 }
