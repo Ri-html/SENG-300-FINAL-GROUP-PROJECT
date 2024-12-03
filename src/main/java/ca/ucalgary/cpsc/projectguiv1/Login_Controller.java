b04d2e3bcc7599517757f59c2cd209d8aa02bcdf
TerrificMage <123597466+TerrificMage@users.noreply.github.com>
2024-12-02 20:20:49 -0700

    30185344 pressing the button takes you to home screen

--- src/main/java/ca/ucalgary/cpsc/projectguiv1/Login_Controller.java
+++ src/main/java/ca/ucalgary/cpsc/projectguiv1/Login_Controller.java
@@ -1,11 +1,9 @@
 package ca.ucalgary.cpsc.projectguiv1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -14,35 +12,23 @@ import java.io.IOException;
 public class Login_Controller {
 
     @FXML
-    Label mainTitle;
+    private GridPane identity;
 
     @FXML
-    TextField usernameTxtField;
-
-    @FXML
-    Label usernameLbl;
-
-    @FXML
-    Button loginButton;
-
-    @FXML
-    GridPane identity;
-
-    @Deprecated
-    public Login_Controller(){ // This class
-
-    }
-    public void signupLinkFxn() throws IOException { // Switch to sign up page
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Signup.fxml"));
+    public void loginButtonFxn(ActionEvent event) throws IOException {
+        // Navigate to the Home Page
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Homepage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        Stage newStg = new Stage();
-        newStg.sizeToScene();
-        newStg.setTitle("Sign Up");
-        newStg.setScene(scene);
-        newStg.show();
-        Stage stgWindw = (Stage) this.identity.getScene().getWindow();
-        stgWindw.close();
-
+        Stage currentStage = (Stage) identity.getScene().getWindow();
+        currentStage.setScene(scene);
     }
 
+    @FXML
+    public void signupLinkFxn(ActionEvent event) throws IOException {
+        // Navigate to the Sign Up Page
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Signup.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage currentStage = (Stage) identity.getScene().getWindow();
+        currentStage.setScene(scene);
+    }
 }
