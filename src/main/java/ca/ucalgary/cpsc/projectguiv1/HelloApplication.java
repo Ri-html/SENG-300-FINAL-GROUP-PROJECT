591d4ac78fbacff56c050920e47e7fa1be207739
Aaron Sheikh <aaronaltaf.sheikh@ucalgary.ca>
2024-12-03 16:41:00 -0700

    30219429: Logging in a dummy user and get their current data in manage profile and updating it

--- src/main/java/ca/ucalgary/cpsc/projectguiv1/HelloApplication.java
+++ src/main/java/ca/ucalgary/cpsc/projectguiv1/HelloApplication.java
@@ -1,5 +1,6 @@
 package ca.ucalgary.cpsc.projectguiv1;
 
+import authProfile.User;
 import authProfile.UserDatabase;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,11 +10,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-
-    public static UserDatabase usrDb = new UserDatabase();
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Homepage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("TicTacToe_Game_Screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Login");
         stage.setScene(scene);
@@ -21,6 +20,12 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        UserDatabase userDb = UserDatabase.getInstance(); // Ensure UserDatabase is a singleton
+        User dummyUser = new User("Dummy Regald", "dummySlame", "dummy@gmail.com");
+        dummyUser.setPassword("securepass");
+        userDb.addUser(dummyUser);
+        dummyUser.loginUser();
+        userDb.setCurrentUser(dummyUser);
         launch();
     }
 }
