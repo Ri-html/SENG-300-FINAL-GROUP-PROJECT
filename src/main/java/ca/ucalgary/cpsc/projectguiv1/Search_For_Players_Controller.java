d42a29ac7ccca0fa9290c9846f1878b095397ec2
Sowmithra Suresh Kumar <sowmithra.sureshkuma@ucalgary.ca>
2024-12-04 16:41:15 -0700

    30113727: Search for players is working, but only for the full name/email of users.

--- src/main/java/ca/ucalgary/cpsc/projectguiv1/Search_For_Players_Controller.java
+++ src/main/java/ca/ucalgary/cpsc/projectguiv1/Search_For_Players_Controller.java
@@ -1,6 +1,9 @@
 package ca.ucalgary.cpsc.projectguiv1;
 
+import UserAndProfile.PlayerProfile;
 import UserAndProfile.ProfileSearch;
+import UserAndProfile.User;
+import UserAndProfile.UserDatabase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -39,27 +42,36 @@ public class Search_For_Players_Controller {
     AnchorPane identity;
 
 
+    UserDatabase db = UserDatabase.getInstance();
+    //private ProfileSearch profileSearch = new ProfileSearch(db);
 
-    private ProfileSearch profileSearch;
+    public void onSearch() {
+        resultView.getItems().clear(); // clears out previous search
 
-//    public Search_For_Players_Controller(ProfileSearch profileSearch){
-//        this.profileSearch = profileSearch;
-//    }
+        String searchTerm = searchBar.getText();
+        if (searchTerm == null || searchTerm.isEmpty()) {
+            resultLabel.setText("Please enter a username or email."); // error message
+        }
+        searchBar.clear();
 
-//    @FXML
-//    private void onSearch(ActionEvent event) {
-//        resultView.getItems().clear(); // clears out previous search
-//        String searchTerm = searchBar.getText();
-//        if (searchTerm == null || searchTerm.isEmpty()) {
-//            resultLabel.setText("Please enter a username or email."); // error message
-//            return;
-//        }
-//
-//        profileSearch.searchProfile(searchTerm);
-//
-//        resultView.getItems().addAll(profileSearch.searchProfile(searchBar.getText()));
-//
-//    }
+        User user = db.searchByUsername(searchTerm);
+        if(user != null){
+            resultView.getItems().add(user.getUsername());
+            resultLabel.setText("");
+        }
+
+        user = db.searchByEmail(searchTerm);
+        if(user != null){
+            resultView.getItems().add(user.getUsername());
+            resultLabel.setText("");
+        }
+
+        if(resultView.getItems().isEmpty() && !searchTerm.isEmpty()){
+            resultView.getItems().add("No profile found for the search term.");
+            resultLabel.setText("");
+        }
+
+    }
 
     public void exitBtnFunc() throws IOException {
         String file = "Homepage.fxml";
@@ -68,7 +80,7 @@ public class Search_For_Players_Controller {
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         Stage newStg = new Stage();
         newStg.sizeToScene();
-        newStg.setTitle("Playing Connect 4!");
+        newStg.setTitle("Homepage");
         newStg.setScene(scene);
         newStg.show();
         Stage stgWindw = (Stage) this.identity.getScene().getWindow();
