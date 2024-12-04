e5eae778c06472116cd0faed2c292ca4d848db94
Aaron Sheikh <aaronaltaf.sheikh@ucalgary.ca>
2024-12-03 17:06:31 -0700

    30219429: Making the connect4 game screen work

--- src/main/java/ca/ucalgary/cpsc/projectguiv1/HelloApplication.java
+++ src/main/java/ca/ucalgary/cpsc/projectguiv1/HelloApplication.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("TicTacToe_Game_Screen.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Connect4_Game_Screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Login");
         stage.setScene(scene);
