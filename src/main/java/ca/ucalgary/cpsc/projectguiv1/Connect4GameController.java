25cd68eed492c5ff5132c40c67135bd78bb3bd59
Aaron Sheikh <aaronaltaf.sheikh@ucalgary.ca>
2024-12-04 21:12:58 -0700

    30219429: Showing and updating the stats of opponent and user after a win or loss

--- src/main/java/ca/ucalgary/cpsc/projectguiv1/Connect4GameController.java
+++ src/main/java/ca/ucalgary/cpsc/projectguiv1/Connect4GameController.java
@@ -3,6 +3,8 @@ package ca.ucalgary.cpsc.projectguiv1;
 import UserAndProfile.User;
 import UserAndProfile.UserDatabase;
 import gameLogic.ConnectFour;
+import leaderboard.connect4Leaderboard.Connect4Leaderboard;
+import leaderboard.tictactoeLeaderboard.TicTacToeLeaderboard;
 import network.Network;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -32,6 +34,7 @@ public class Connect4GameController {
     private User playerOne;
     private User playerTwo;
     private boolean oneAlert = false;
+    private Connect4Leaderboard connect4Lead;
 
     // FXML Components
     @FXML
@@ -79,12 +82,13 @@ public class Connect4GameController {
             playerTwo = connect4Players.get(1);
         }
 
+        connect4Lead = Connect4Leaderboard.getInstance();
+
         connectFourGame = new ConnectFour(2);
         connectFourGame.addPlayer(playerOne.getUsername());
         connectFourGame.addPlayer(playerTwo.getUsername());
 
-        Random random = new Random();
-        connectFourGame.setCurrentPlayer(random.nextInt(2));
+        connectFourGame.setCurrentPlayer(0);
 
         infoLabel.setText("Click to start!");
 
@@ -107,8 +111,18 @@ public class Connect4GameController {
         player2Name.setText(playerTwo.getUsername());
         winLabelP1.setText("Win: " + playerOne.getPlayerProfile().getConnectFourProfile().getTotalWins());
         winLabelP2.setText("Win: " + playerTwo.getPlayerProfile().getConnectFourProfile().getTotalWins());
-        rankLabelP1.setText("Rank: " + playerOne.getPlayerProfile().getConnectFourProfile().getScoreRank());
-        rankLabelP2.setText("Rank: " + playerTwo.getPlayerProfile().getConnectFourProfile().getScoreRank());
+
+        if(connect4Lead.getPlayerRank(playerOne.getUsername()) != -1) {
+            this.rankLabelP1.setText("Rank: " + connect4Lead.getPlayerRank(playerOne.getUsername()));
+        }else{
+            this.rankLabelP1.setText("Rank: 0");
+        }
+
+        if(connect4Lead.getPlayerRank(playerTwo.getUsername()) != -1) {
+            this.rankLabelP2.setText("Rank: " + connect4Lead.getPlayerRank(playerTwo.getUsername()));
+        }else{
+            this.rankLabelP2.setText("Rank: 0");
+        }
 
     }
 
@@ -148,6 +162,40 @@ public class Connect4GameController {
 
     }
 
+//    private void makeRandMove() {
+//        Random rand = new Random();
+//        int col = rand.nextInt(COLUMNS);
+//        int row = getAvailableRow(col);
+//        int[] moves = {col, row};
+//        boolean isValid = connectFourGame.validateMove(moves);
+//
+//        if (!isValid) {
+//            infoLabel.setText("Column is full. Try a different column.");
+//            return;
+//        }
+//
+//        Color playerColor;
+//        if (connectFourGame.getCurrentPlayer().equals(playerOne.getUsername())) {
+//            playerColor = Color.YELLOW; // Player 1's color (Yellow)
+//
+//            connectFourGame.makeMove(moves);
+//            connectFourGame.switchCurrentPlayer();
+//            board[row][col].setFill(playerColor);
+//
+//            checkEndCon();
+//
+//        } else {
+//            playerColor = Color.RED; // Player 1's color (Yellow)
+//
+//            connectFourGame.makeMove(moves);
+//            connectFourGame.switchCurrentPlayer();
+//            board[row][col].setFill(playerColor);
+//
+//            checkEndCon();
+//        }
+//
+//    }
+
     private void makeRandMove() {
         Random rand = new Random();
         int col = rand.nextInt(COLUMNS);
@@ -161,24 +209,13 @@ public class Connect4GameController {
         }
 
         Color playerColor;
-        if (connectFourGame.getCurrentPlayer().equals(playerOne.getUsername())) {
-            playerColor = Color.YELLOW; // Player 1's color (Yellow)
+        playerColor = Color.RED;
 
-            connectFourGame.makeMove(moves);
-            connectFourGame.switchCurrentPlayer();
-            board[row][col].setFill(playerColor);
+        connectFourGame.makeMove(moves);
+        connectFourGame.switchCurrentPlayer();
+        board[row][col].setFill(playerColor);
+        checkEndCon();
 
-            checkEndCon();
-
-        } else {
-            playerColor = Color.RED; // Player 1's color (Yellow)
-
-            connectFourGame.makeMove(moves);
-            connectFourGame.switchCurrentPlayer();
-            board[row][col].setFill(playerColor);
-
-            checkEndCon();
-        }
 
     }
 
@@ -190,7 +227,7 @@ public class Connect4GameController {
                 alert.setHeaderText("Red Wins!");
                 alert.show();
                 alert.setOnHidden(dialogEvent -> {
-                    saveEndData('W', 1);
+                    saveEndData('W', 2);
                     try {
                         exitBtnFunc();
                     } catch (IOException ioe) {
@@ -205,7 +242,7 @@ public class Connect4GameController {
                 alert.setHeaderText("Yellow Wins!");
                 alert.show();
                 alert.setOnHidden(dialogEvent -> {
-                    saveEndData('W', 2);
+                    saveEndData('W', 1);
                     try {
                         exitBtnFunc();
                     } catch (IOException ioe) {
@@ -217,16 +254,7 @@ public class Connect4GameController {
         }
     }
 
-
-//    private void checkEndCon() {
-//        if (gameEnded) return; // Prevent further checks once the game has ended
-//
-//        if (connectFourGame.checkWin() == 1 || connectFourGame.checkWin() == 2) {
-//            gameEnded = true; // Mark the game as ended
-//            showWinnerAlert();
-//        }
-//    }
-
+    
     // Get the available row for a specific column
     private int getAvailableRow(int col) {
         for (int row = ROWS - 1; row >= 0; row--) {
@@ -236,23 +264,7 @@ public class Connect4GameController {
         }
         return -1; // Column is full
     }
-
-
-    // Show an alert when a player wins
-//    private void showWinnerAlert() {
-//        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-//        alert.setTitle("Winner!");
-//        alert.setHeaderText((connectFourGame.getCurrentPlayer().equals(playerOne.getUsername()) ? "Player 1" : "Player 2") + " won!");
-//        alert.showAndWait().ifPresent(response -> {
-//            if (response == ButtonType.OK) {
-//                try {
-//                    exitBtnFunc();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//    }
+    
 
     // Exit the game and return to the main menu
     @FXML
@@ -268,7 +280,6 @@ public class Connect4GameController {
         currentStage.close();
     }
 
-    // Placeholder for chat feature (not implemented)
     @FXML
     public void sendBtnFunc() {
         // Get the text from the input field
@@ -297,17 +308,24 @@ public class Connect4GameController {
 
     public void saveEndData(char result, int player) { // Might have to change
 
-        if ((result == 'W') && (player == 1)) {
+        if((result == 'W') && (player == 1)) { // Update all the backend upon current user winning
             playerOne.getPlayerProfile().getConnectFourProfile().setTotalWins(playerOne.getPlayerProfile().getConnectFourProfile().getTotalWins() + 1);
             playerTwo.getPlayerProfile().getConnectFourProfile().setTotalLosses(playerTwo.getPlayerProfile().getConnectFourProfile().getTotalLosses() + 1);
             playerOne.getPlayerProfile().getConnectFourProfile().updateRanking(playerOne.getPlayerProfile().getConnectFourProfile().getScoreRank(), playerOne.getPlayerProfile().getConnectFourProfile().getWinRateRank());
-            playerTwo.getPlayerProfile().getConnectFourProfile().updateGameHistory(playerTwo.getUsername(), "W", 1);
+            playerOne.getPlayerProfile().getConnectFourProfile().updateGameHistoryReal(playerTwo.getUsername(), "W",  1);
+            playerTwo.getPlayerProfile().getConnectFourProfile().updateGameHistoryReal(playerOne.getUsername(), "L",  -1);
+            connect4Lead.recordWin(playerOne.getUsername());
+            connect4Lead.recordLoss(playerTwo.getUsername());
 
-        } else if ((result == 'W') && (player == 2)) {
-            playerTwo.getPlayerProfile().getConnectFourProfile().setTotalWins(playerTwo.getPlayerProfile().getConnectFourProfile().getTotalWins() + 1);
-            playerOne.getPlayerProfile().getConnectFourProfile().setTotalLosses(playerOne.getPlayerProfile().getConnectFourProfile().getTotalLosses() + 1);
+        }else if ((result == 'W') && (player == 2)){ // Update all the backend upon current user winning
+            playerTwo.getPlayerProfile().getConnectFourProfile().setTotalWins(playerTwo.getPlayerProfile().getTicTacToeProfile().getTotalWins() + 1);
+            playerOne.getPlayerProfile().getConnectFourProfile().setTotalLosses(playerOne.getPlayerProfile().getTicTacToeProfile().getTotalLosses() + 1);
             playerTwo.getPlayerProfile().getConnectFourProfile().updateRanking(playerTwo.getPlayerProfile().getConnectFourProfile().getScoreRank(), playerTwo.getPlayerProfile().getConnectFourProfile().getWinRateRank());
-            playerOne.getPlayerProfile().getConnectFourProfile().updateGameHistory(playerOne.getUsername(), "W", 1);
+            playerTwo.getPlayerProfile().getConnectFourProfile().updateGameHistoryReal(playerOne.getUsername(), "W", 1);
+            playerOne.getPlayerProfile().getConnectFourProfile().updateGameHistoryReal(playerTwo.getUsername(), "L", -1);
+            connect4Lead.recordWin(playerTwo.getUsername());
+            connect4Lead.recordLoss(playerOne.getUsername());
+
         }
     }
 }
