161726f0d834433c4d1d7faaa3c3c1c656114d03
Connor GAVLIN <connor.gavlin@ucalgary.ca>
2024-12-05 19:47:40 -0700

    30071699 tests for valid moves of king piece

--- test/gameLogic/piece/chessPiece/KingTest.java
+++ test/gameLogic/piece/chessPiece/KingTest.java
@@ -9,9 +9,31 @@ import java.util.Arrays;
 
 
 public class KingTest {
+    
     @Test
-    public void kingMoveTest(){
+    public void kingValidMovesTest1(){
+        Chess chess = new Chess();
+        //move pawn to be able to access king
+        ((AbstractChessPiece)chess.getBoard()[1][4]).makeMove(chess.getBoard(), 3, 4);
+        System.out.println(Arrays.deepToString(chess.getBoard()));
+        AbstractChessPiece k = (AbstractChessPiece)chess.getBoard()[0][4];
+        int[][] valids = k.getValidMoves(chess.getBoard());
+        int[][] expecteds = new int[][]{
+                {1,4},};
+        assertArrayEquals(valids, expecteds);
+    }
 
+    @Test
+    public void kingValidMovesTest2(){
+        //test king movement is blocked correctly
+        Chess chess = new Chess();
+        System.out.println(Arrays.deepToString(chess.getBoard()));
+        AbstractChessPiece k = (AbstractChessPiece)chess.getBoard()[0][4];
+        int[][] valids = k.getValidMoves(chess.getBoard());
+        //Expect no valid moves
+        int[][] expecteds = new int[][]{
+                };
+        assertArrayEquals(valids, expecteds);
     }
 
 }
