beac7721df861f3292e9a0ad46b27cb1d059647a
Pravin Kumar <pravin.kumar1@ucalgary.ca>
2024-12-06 21:52:44 -0700

    10098687: added javadoc comments to the player class

--- src/main/java/leaderboard/matchmaking_LB/Player.java
+++ src/main/java/leaderboard/matchmaking_LB/Player.java
@@ -6,6 +6,11 @@ public class Player {
     private int skillLevel;
     private String status; // Available, In-Game, etc.
 
+    /**
+     * Constructor to create a new player.
+     * @param username The username of the player.
+     * @param skillLevel The skill level of the player.
+     */
     public Player(String username, int skillLevel) {
         this.playerID = generatePlayerID();
         this.username = username;
@@ -13,31 +18,60 @@ public class Player {
         this.status = "Available";
     }
 
+    /**
+     * Generates a unique player ID.
+     * @return A unique player ID.
+     */
     protected String generatePlayerID() {
-        // Generate a unique player ID (e.g., UUID)
         return java.util.UUID.randomUUID().toString();
     }
 
+
+    /**
+     * Gets the player ID.
+     * @return The player ID.
+     */
     public String getPlayerID() {
         return playerID;
     }
 
+   /**
+     * Gets the username of the player.
+     * @return The username of the player.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Gets the skill level of the player.
+     * @return The skill level of the player.
+     */
     public int getSkillLevel() {
         return skillLevel;
     }
 
+    /**
+     * Gets the status of the player.
+     * @return The status of the player (e.g., Available, In-Game).
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Sets the status of the player.
+     * @param status The new status of the player.
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * Checks if this player is equal to another object.
+     * @param obj The object to compare.
+     * @return True if the object is a Player and has the same player ID, false otherwise.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -46,6 +80,10 @@ public class Player {
         return playerID.equals(player.playerID);
     }
 
+    /**
+     * Generates a hash code for the player.
+     * @return The hash code of the player.
+     */
     @Override
     public int hashCode() {
         return playerID.hashCode();
