67f5a09471f8b964ea487d400c7375a311b6cd5d
Alex Aghajanov <alex.aghajanov@ucalgary.ca>
2024-12-06 21:36:12 -0700

    30207209: Added Javadocs to GameRecord

--- src/main/java/UserAndProfile/GameRecord.java
+++ src/main/java/UserAndProfile/GameRecord.java
@@ -1,12 +1,24 @@
 package UserAndProfile;
 
+/**
+ * A class representing a record of a single game, including details about the game type,
+ * opponent, result, and score.
+ */
 public class GameRecord {
-    private String game;
-    private String opponent;  // 对手的名字
-    private String result;    // 比赛结果（如 "Win", "Lose", "Draw"）
-    private int score;        // 游戏得分
 
-    // Constructor
+    private String game; // The name of the game
+    private String opponent; // The name of the opponent
+    private String result; // The result of the game (e.g., "Win", "Lose", "Draw")
+    private int score; // The score achieved in the game
+
+    /**
+     * Constructs a GameRecord object with the specified details.
+     * 
+     * @param game The name of the game.
+     * @param opponent The name of the opponent.
+     * @param result The result of the game (e.g., "Win", "Lose", "Draw").
+     * @param score The score achieved in the game.
+     */
     public GameRecord(String game, String opponent, String result, int score) {
         this.game = game;
         this.opponent = opponent;
@@ -14,29 +26,52 @@ public class GameRecord {
         this.score = score;
     }
 
-    // Getters
+    /**
+     * Gets the name of the opponent.
+     * 
+     * @return The name of the opponent.
+     */
     public String getOpponent() {
         return opponent;
     }
 
+    /**
+     * Gets the result of the game.
+     * 
+     * @return The result of the game (e.g., "Win", "Lose", "Draw").
+     */
     public String getResult() {
         return result;
     }
 
+    /**
+     * Gets the score achieved in the game.
+     * 
+     * @return The score of the game.
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * Gets the name of the game.
+     * 
+     * @return The name of the game.
+     */
     public String getGame() {
         return game;
     }
 
-    // toString 方法，方便打印记录
+    /**
+     * Returns a string representation of the game record, including the game, opponent, result, and score.
+     * 
+     * @return A formatted string representation of the game record.
+     */
     @Override
     public String toString() {
         return "Game = " + game + "\n" +
-                "Opponent = " + opponent + "\n" +
-                "Result = " + result + "\n" +
-                "Score = " + score;
+               "Opponent = " + opponent + "\n" +
+               "Result = " + result + "\n" +
+               "Score = " + score;
     }
 }
